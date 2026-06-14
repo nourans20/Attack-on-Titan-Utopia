9671da78954f8300690a94c4e4f4fde0f9982d4d
nourans20 <nouran.samieh@student.guc.edu.eg>
2026-06-15 00:47:45 +0300

    Added Lane properties

--- AttackOnTitanUtopia/src/game/engine/lanes/Lane.java
+++ AttackOnTitanUtopia/src/game/engine/lanes/Lane.java
@@ -50,4 +50,58 @@ public class Lane implements Comparable<Lane> {
         return Integer.compare(this.dangerLevel, o.dangerLevel);
     }
 
+    // Methods
+
+    public void addTitan(Titan titan) {
+        titans.add(titan);
+    }
+
+    public void addWeapon(Weapon weapon) {
+        weapons.add(weapon);
+    }
+
+    public void moveLaneTitans() {
+        int size = titans.size();
+        ArrayList<Titan> processed = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Titan titan = titans.remove();
+            if (!titan.hasReachedTarget())
+                titan.move();
+            processed.add(titan);
+        }
+        titans.addAll(processed);
+    }
+
+    public int performLaneTitansAttacks() {
+        int resourcesGained = 0;
+        int size = titans.size();
+        ArrayList<Titan> processed = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Titan titan = titans.remove();
+            if (!titan.hasReachedTarget())
+                resourcesGained += titan.attack(laneWall);
+            processed.add(titan);
+        }
+        titans.addAll(processed);
+        return resourcesGained;
+    }
+
+    public int performLaneWeaponsAttacks() {
+        int resourcesGained = 0;
+        for (Weapon weapon : weapons)
+            resourcesGained += weapon.turnAttack(titans);
+        return resourcesGained;
+    }
+
+    public boolean isLaneLost() {
+        return laneWall.getCurrentHealth() <= 0;
+    }
+
+    public void updateLaneDangerLevel() {
+        int newDangerLevel = 0;
+        for (Titan titan : titans)
+            newDangerLevel += titan.getDangerLevel();
+        setDangerLevel(newDangerLevel);
+    }
+
 }
